c9c50e6362e55a5c6aa4fbb73a6b0ff1f5cb3f5d
Tomasz Wójcik <wojcik.tomek@gmail.com>
2013-07-31 17:27:32 +0200

    Introduce git committer. Move main body to debug function.

---
+++ src/git_committer.cpp
@@ -0,0 +1,50 @@
+#include "git_committer.h"
+
+#include <iostream>
+#include <algorithm>
+#include <unistd.h>
+
+using namespace std;
+
+GitCommitter::GitCommitter() {
+}
+
+GitCommitter::~GitCommitter() {
+
+}
+
+void GitCommitter::addFile(string* filename) {
+    if (find(files.begin(), files.end(), filename) == files.end()) {
+        files.push_back(filename);
+    }
+}
+
+void GitCommitter::removeFile(string* filename) {
+    vector<string*>::iterator it = find(files.begin(), files.end(), filename);
+    if (it != files.end()) {
+        files.erase(it);
+    }
+}
+
+void GitCommitter::commit(string message) {
+    gitAdd();
+    char message_delimiter = '\'';
+    if (message.find(message_delimiter) != string::npos) {
+        message_delimiter = '\"';
+    }
+    string git_command = "git commit -m ";
+    git_command += message_delimiter;
+    git_command += message;
+    git_command += message_delimiter;
+    system(git_command.c_str());
+}
+
+void GitCommitter::gitAdd() {
+    vector<string*>::iterator it;
+    string git_command = "git add ";
+    for (it = files.begin(); it != files.end(); it++) {
+        string command(git_command);
+        command += (**it);
+        system(command.c_str());
+    }
+}
